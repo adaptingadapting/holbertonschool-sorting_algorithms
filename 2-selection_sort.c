0509c16c861db07485e8763fe65b79cf3e38ba57
adaptingadapting <elpatoninjasuperninja@gmail.com>
2023-01-06 09:42:43 -0300

    hotfix

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,28 +8,33 @@
 
 void selection_sort(int *array, size_t size)
 {
-	int current_smaller = 0;
+	int current_smaller;
 	int array_pos;
+	int changed = 0;
 	int aux;
 	size_t i;
 	size_t j;
 
+	if (!array)
+		return;
 	for (j = 0; j < size - 1; j++)
 	{
 		current_smaller = array[j];
 		for (i = j; i < size; i++)
 		{
-			if (array[i] < current_smaller)
+			if (array[j] > array[i] && array[i] < current_smaller)
 			{
 				current_smaller = array[i];
 				array_pos = i;
+				changed = 1;
 			}
 		}
-		if (array[j] > array[array_pos])
+		if (changed)
 		{
 			aux = array[j];
 			array[j] = array[array_pos];
 			array[array_pos] = aux;
+			changed = 0;
 			print_array(array, size);
 		}
 	}
