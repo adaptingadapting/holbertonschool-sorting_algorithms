a3770ce341e779820c8e12e36eb04691e16eed7e
adaptingadapting <elpatoninjasuperninja@gmail.com>
2023-01-07 08:40:58 -0300

    quick sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,91 @@
+#include "sort.h"
+
+/**
+ * quick_sort - uses the quick sort to sort
+ * @array: array to sort
+ * @size: size of the array
+ */
+
+void quick_sort(int *array, size_t size)
+{
+	lo_version(array, 0, size, size);
+}
+
+/**
+ * lo_version - i feel like a had to to a version
+ * with the lo parameter so it would work recursively
+ * i tried as hard as i could to avoid this but
+ * i dont see any other way really, too bad, i know there
+ * probably is one...
+ * @array: array to be sorted
+ * @lo: lowest in the array you want to go, after the
+ * last partition this is going to be placed.
+ * @hi: highest in the array you want to go, its going
+ * to be the last item in this case, so size
+ * @size: size of the array for printintg
+ */
+
+void lo_version(int *array, int lo, int high, size_t size)
+{
+	int pivot;  
+
+	if (lo < high)
+	{
+		pivot = partition(array, lo, high, size);
+		lo_version(array, lo, pivot - 1, size);
+		lo_version(array, pivot + 1, high, size);
+	}
+}
+
+/**
+ * partition - does most of the work of the function
+ * partition orders an array based on a "pivot", and puts
+ * numbers that are smaller than it to the left or behind in
+ * the array and numbers that are bigger to the right, this
+ * if applied recursively orders an array in n log n time
+ * @array: array to sort
+ * @lo: lowest you want to go in the array
+ * @hi: highest index in the array (size)
+ * @size: size of the array, printintg prupose
+ * Return: returns the new position of the pivot
+ */
+
+int partition(int *array, int lo, int hi, size_t size)
+{
+	int i = (lo -1);
+	int j;
+	int pivot = array[hi];
+
+	for (j = lo; j <= hi - 1; j++)
+	{
+		if (array[j] < pivot)
+		{
+			i++;
+			swap_int(&array[i], &array[j]);
+			if (i != j)
+			{
+				print_array(array, size + 1);
+			}
+		}
+	}
+	swap_int(&array[i + 1], &array[hi]);	
+	if (i + 1 != hi)
+	{
+		print_array(array, size + 1);
+	}
+	return (i + 1);
+}
+
+/**
+ * swap_int - swap int for pointers and strings
+ * @a: int a
+ * @b: int b
+ */
+
+void swap_int(int *a, int *b)
+{
+	int i = *a;
+
+	*a = *b;
+	*b = i;
+}
--- sort.h
+++ sort.h
@@ -21,6 +21,10 @@ typedef struct listint_s
 	struct listint_s *next;
 }	listint_t;
 
+void lo_version(int *array, int lo, int high, size_t size);
+int partition(int *array, int lo, int high, size_t size);
+void swap_int(int *a, int *b);
+void quick_sort(int *array, size_t size);
 void swap_nodes(listint_t **h, listint_t **swap1, listint_t *swao2);
 void selection_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
